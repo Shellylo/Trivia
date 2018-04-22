1ee040cebb56d0280179c110e281ffecaa3e1f9a
netanel1951 <netanelesman@gmail.com>
2018-04-22 23:12:41 +0300

    fixed linkage error

--- Trivia/SqliteDatabase.h
+++ Trivia/SqliteDatabase.h
@@ -22,7 +22,7 @@ public:
 	bool doesUserExist(std::string username) override;
 	void addUser(std::string username, std::string pass, std::string email) override;
 	bool passMatches(std::string username, std::string pass) override { return true; }
-	std::list<Question> getQuestions(int num) override;
+	std::list<Question> getQuestions(int num) override { return std::list<Question>(); };
 private:
 	sqlite3* db;
 
