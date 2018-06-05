29f0eab90e76b2aec7a543fa80d6eb764c260b6f
netanel1951 <netanelesman@gmail.com>
2018-06-05 16:43:52 +0300

    v4 finished

--- Trivia/Helper.h
+++ Trivia/Helper.h
@@ -1,5 +1,4 @@
-#ifndef HELPER
-#define HELPER
+#pragma once
 #include <vector>
 #include <iostream>
 #include "LoggedUser.h"
@@ -11,18 +10,5 @@ namespace Helper
 		Input: vector of logged users and a user
 		Output: the iterator to the logged user or the iterator end if not found	
 	*/
-	std::vector<LoggedUser>::iterator find(std::vector<LoggedUser>& vec, LoggedUser value)
-	{
-		std::vector<LoggedUser>::iterator ret = vec.end();
-		for (std::vector<LoggedUser>::iterator it = vec.begin(); it != vec.end() && ret == vec.end(); it++)
-		{
-			if (*it == value)
-			{
-				ret = it;
-			}
-		}
-		return ret;
-	}
+	std::vector<LoggedUser>::iterator find(std::vector<LoggedUser>& vec, LoggedUser value);
 }
-
-#endif
