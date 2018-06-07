163c1b9e87952f9400a724c6476651c482db96c4
netanel1951 <netanelesman@gmail.com>
2018-06-07 23:34:51 +0300

    Merged

--- TriviaClient/TriviaClient/JsonResponsePacketDeserializer.cs
+++ TriviaClient/TriviaClient/JsonResponsePacketDeserializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TriviaClient
 {
@@ -32,80 +33,160 @@ namespace TriviaClient
         public struct LoginResponse
         {
             public uint status;
+
+            public LoginResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct SignupResponse
         {
             public uint status;
+
+            public SignupResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct LogoutResponse
         {
             public uint status;
+
+            public LogoutResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct GetRoomsResponse
         {
             public uint status;
-            public std::vector<RoomData> rooms;
+            public List<RoomData> rooms;
+
+            public GetRoomsResponse(uint status, List<RoomData> rooms)
+            {
+                this.status = status;
+                this.rooms = rooms;
+            }
         };
 
         public struct GetPlayersInRoomResponse
         {
             public uint status;
-            public std::vector<string> users;
+            public List<string> users;
+
+            public GetPlayersInRoomResponse(uint status, List<string> users)
+            {
+                this.status = status;
+                this.users = users;
+            }
         };
 
         public struct JoinRoomResponse
         {
             public uint status;
+
+            public JoinRoomResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct CreateRoomResponse
         {
             public uint status;
+
+            public CreateRoomResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct HighscoreResponse
         {
             public uint status;
-            public std::vector<Highscore> highscores;
+            public List<Highscore> highscores;
+
+            public HighscoreResponse(uint status, List<Highscore> highscores)
+            {
+                this.status = status;
+                this.highscores = highscores;
+            }
         };
 
         public struct CloseRoomResponse
         {
             public uint status;
+
+            public CloseRoomResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct StartGameResponse
         {
             public uint status;
+
+            public StartGameResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct GetRoomStateResponse
         {
             public uint status;
             public uint roomStatus;
-            public std::vector<string> players;
+            public List<string> players;
             public uint questionCount;
             public uint answerTimeout;
+
+            public GetRoomStateResponse(uint status, uint roomStatus, List<string> players, uint questionCount, uint answerTimeout)
+            {
+                this.status = status;
+                this.roomStatus = roomStatus;
+                this.players = players;
+                this.questionCount = questionCount;
+                this.answerTimeout = answerTimeout;
+            }
+
         };
 
         public struct LeaveRoomResponse
         {
             public uint status;
+
+            public LeaveRoomResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct JoinGameResponse
         {
             public uint status;
+
+            public JoinGameResponse(uint status)
+            {
+                this.status = status;
+            }
         };
 
         public struct GetQuestionResponse
         {
             public uint status;
             public string question;
-            public std::vector<string> answers;
+            public List<string> answers;
+
+            public GetQuestionResponse(uint status, string question, List<string> answers)
+            {
+                this.status = status;
+                this.question = question;
+                this.answers = answers;
+            }
         };
 
         public struct SubmitAnswerResponse
@@ -113,25 +194,90 @@ namespace TriviaClient
             public uint status;
             public string correctAnswer;
             public bool hasFinished;
+
+            public SubmitAnswerResponse(uint status, string correctAnswer, bool hasFinished)
+            {
+                this.status = status;
+                this.correctAnswer = correctAnswer;
+                this.hasFinished = hasFinished;
+            }
         };
 
         public struct GetGameResultsResponse
         {
             public uint status;
-            public std::vector<PlayerResults> results;
+            public List<PlayerResults> results;
+
+            public GetGameResultsResponse(uint status, List<PlayerResults> results)
+            {
+                this.status = status;
+                this.results = results;
+            }
         };
 
         public struct LeaveGameResponse
         {
-            uint status;
+            public uint status;
+
+            public LeaveGameResponse(uint status)
+            {
+                this.status = status;
+            }
+        };
+
+        public struct Highscore
+        {
+            public string username;
+            public int score;
+            public string time;
+
+            public Highscore(string username, int score, string time)
+            {
+                this.username = username;
+                this.score = score;
+                this.time = time;
+            }
+        };
+
+        public struct RoomData
+        {
+            public uint id;
+            public string name;
+            public uint maxPlayers; //was string changed to int
+            public uint questionCount; //added this
+            public uint timePerQuestion;
+            public uint roomState; // 1 - waiting, 2 - closed, 3 - game started
+
+            public RoomData(uint id, string name, uint maxPlayers, uint questionCount, uint timePerQuestion, uint roomState)
+            {
+                this.id = id;
+                this.name = name;
+                this.maxPlayers = maxPlayers;
+                this.questionCount = questionCount;
+                this.timePerQuestion = timePerQuestion;
+                this.roomState = roomState;
+            }
         };
 
         public struct PlayerResults
         {
-            string username;
-            uint correctAnswerCount;
-            uint wrongAnswerCount;
-            uint averageAnswerTime;
+            public string username;
+            public uint correctAnswerCount;
+            public uint wrongAnswerCount;
+            public uint averageAnswerTime;
+
+            public PlayerResults(string username, uint correctAnswerCount, uint wrongAnswerCount, uint averageAnswerTime)
+            {
+                this.username = username;
+                this.correctAnswerCount = correctAnswerCount;
+                this.wrongAnswerCount = wrongAnswerCount;
+                this.averageAnswerTime = averageAnswerTime;
+            }
         };
+
+        public static T createStruct<T>(byte[] buff)
+        {
+            return JsonConvert.DeserializeObject<T>(new ASCIIEncoding().GetString(buff));
+        }
     }
 }
